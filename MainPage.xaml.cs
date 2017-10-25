50d46cdbc072117ea4e04d9cdcaa5e1bb8e6fe9c
davidshaek <davidshaek@gmail.com>
2017-10-25 16:15:36 -0500

    Taller

---
+++ MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EspePocket.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        public void OnDoneButtonClicked(object sender, EventArgs args)
+        {
+
+            ((App)Application.Current).SwitchTheme();
+
+        }
+
+        public async void MiEspe(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new miEspe());
+
+        }
+
+       
+        public async void Reglamento(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new Reglamento());
+        }
+        public async void Utilidades(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new Utilidad());
+        }
+        public async void Carreras(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new Carreras());
+        }
+        public async void Matriculacion(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new Matriculacion());
+        }
+        public async void Preguntas(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new Preguntas());
+        }
+    }
+}
---
+++ Reglamento.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EspePocket.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Reglamento : ContentPage
+    {
+        public Reglamento()
+        {
+            Title = "Reglamentos";
+            var browser = new CustomWebView
+            {
+                Uri = "https://goo.gl/wjpD7L",
+            };
+              Content = browser;
+           
+            
+        }
+    }
+
+}
